f17b56779aed96827cc6885c2e9fa6bf0c9e5197
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2024-08-13 00:18:18 +0600

    Create configuration.rb
    Signed-off-by: gitworkflows <118260833+gitworkflows@users.noreply.github.com>

---
+++ lib/pg_adapter/configuration.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+module PgAdapter
+  class Configuration
+    attr_accessor :add_failover_patch, :add_reset_column_information_patch, :reconnect_with_backoff
+
+    def initialize(attrs)
+      self.add_failover_patch = attrs[:add_failover_patch]
+      self.add_reset_column_information_patch = attrs[:add_reset_column_information_patch]
+      self.reconnect_with_backoff = attrs[:reconnect_with_backoff]
+    end
+  end
+
+  def self.configuration
+    @configuration ||= Configuration.new({
+      add_failover_patch: false,
+      add_reset_column_information_patch: false,
+      reconnect_with_backoff: [],
+    })
+  end
+
+  def self.configure
+    yield(configuration)
+  end
+
+  def self.failover_patch?
+    PgAdapter.configuration.add_failover_patch || false
+  end
+
+  def self.reconnect_with_backoff?
+    !PgAdapter.configuration.reconnect_with_backoff.empty?
+  end
+
+  def self.reset_column_information_patch?
+    PgAdapter.configuration.add_reset_column_information_patch || false
+  end
+
+  def self.reset_configuration
+    @configuration = Configuration.new({
+      add_failover_patch: false,
+      add_reset_column_information_patch: false,
+      reconnect_with_backoff: [],
+    })
+  end
+end
