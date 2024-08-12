826f6b8720265a121d62c045ec0cb96cfdf2bc38
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2024-08-13 00:08:00 +0600

    Create spec_helper.rb
    Signed-off-by: gitworkflows <118260833+gitworkflows@users.noreply.github.com>

---
+++ spec/spec_helper.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require "active_record/railtie"
+require "rspec/rails"
+require_relative "../lib/pg-adapter"
+
+RSpec.configure do |config|
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = ".rspec_status"
+
+  # Disable RSpec exposing methods globally on `Module` and `main`
+  config.disable_monkey_patching!
+
+  config.expect_with(:rspec) do |c|
+    c.syntax = :expect
+  end
+
+  config.before(:suite) do
+    ActiveRecord::Base.establish_connection(
+      adapter: "postgresql",
+      host: "localhost",
+      port: 5434,
+      user: "tines",
+      password: "tines",
+      database: "tines",
+    )
+  end
+end
