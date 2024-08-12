397b7d245d4bd5930bd470f0cd21007f7dbb5a3c
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2024-08-13 00:14:05 +0600

    Create pg-adapter.rb
    Signed-off-by: gitworkflows <118260833+gitworkflows@users.noreply.github.com>

---
+++ lib/pg-adapter.rb
@@ -0,0 +1,11 @@
+# rubocop:disable Naming/FileName
+# frozen_string_literal: true
+
+require "pg_adapter/version"
+require "pg_adapter/configuration"
+require "pg_adapter/patch"
+
+module PgAdapter
+end
+
+# rubocop:enable Naming/FileName
